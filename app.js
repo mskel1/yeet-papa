51dc3c2197911efb9a38cb25701fe0c876318042
Cumbie <bcumbie@una.edu>
2023-02-14 16:45:06 -0600

    hammering on mongodb connect

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;  
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://barry:dGtkiX5G9ogB9DUZ@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log("before connection");
+
+client.connect(err => {
+  console.log("in connect method");
+  const collection = client.db("papa").collection("dev-profiles");
+  console.log("decl collection");
+  console.log(collection.findOne()); 
+  client.close();
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World This is Barry 3!')
+  res.send('Hello World This is Barry 3!');
+
+  client.connect(err => {
+    console.log("in connect method");
+    const collection = client.db("papa").collection("dev-profiles");
+    console.log("decl collection");
+    console.log(collection); 
+    client.close();
+  });
+
 })
 
 console.log('in the node console');
